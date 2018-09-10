034f3b873d61d06357fee7bf49e1d55a9ffb5489
yumere <yumere7833@gmail.com>
2018-09-10 15:31:30 +0900

    Complete prepare.py
    1. Make it parallel
    2. Make entire resources

--- prepare.py
+++ prepare.py
@@ -20,13 +20,13 @@ def get_retweet_user(inputs):
     """
     This function searches engaged users which re-tweet the original tweet(tweet_id)
     :param inputs: tuple which have tweet_id and rating(True/False)
-    :return:
+    :return: tuple(engaged_user_vector: np.array, rating: 1/0)
     """
-    tweet_id, rating = inputs
+    tweet_id, user_id, rating = inputs
 
     client = MongoClient(**db_config)
     db: Database = client['fakenews']
-    tweets_collections: Collection = db['tweets_sample']
+    tweets_collections: Collection = db['tweets']
 
     query = tweets_collections.find({"retweeted_status.id": tweet_id}).sort([("created_at", 1)]).limit(limit)
     engaged_users = []
@@ -56,6 +56,7 @@ if __name__ == '__main__':
     parser.add_argument('--config', type=str, help="config file path", required=True)
     parser.add_argument('--max-length', type=int, help="max sequence length", required=True)
     parser.add_argument('--output', type=str, help="output file path", required=True)
+    parser.add_argument('-c', '--cpu', type=int, default=20, metavar="20", help="amount of cpu for multiprocessing")
     args = parser.parse_args()
 
     limit = args.max_length
@@ -64,7 +65,7 @@ if __name__ == '__main__':
     db_config.update(config['db_config'])
     client = MongoClient(**db_config)
     db: Database = client['fakenews']
-    tweets_collections: Collection = db['tweets_sample']
+    tweets_collections: Collection = db['tweets']
     users_collection: Collection = db['users']
 
     for user in users_collection.find({"rating": {"$ne": None}}):
@@ -74,7 +75,7 @@ if __name__ == '__main__':
     print("[+] Total users given labels: {:,}".format(len(users)))
 
     unique_tweets_filter = {
-        "retweeted_status": {
+        "retweeted_status.id": {
             "$exists": False
         },
         "in_reply_to_status_id": {
@@ -82,23 +83,19 @@ if __name__ == '__main__':
         }
     }
 
-    tqdm.write("[+] Total tweets: {:,}".format(tweets_collections.count_documents(unique_tweets_filter)))
+    tweets_count = tweets_collections.count_documents(unique_tweets_filter)
+    tqdm.write("[+] Total tweets: {:,}".format(tweets_count))
 
     tweets = []
-
     # Get original tweets except for re-tweets and rely tweets
     loader = tweets_collections.find(unique_tweets_filter, {'_id': 1, 'user': 1})
     # Filter out users whether each user has a ground truth label
     loader = map(lambda x: (x['_id'], x['user']['id'], users.get(x['user']['id'], {}).get('rating', None)), loader)
     loader = filter(lambda x: x[2] is not None, loader)
 
-    # For Debug
-    # TODO: Make it parallel
-    with open(args.output, "wt") as output_f, tqdm(desc="tweets") as pbar:
-        for i, (tweet_id, user_id, rating) in enumerate(loader):
+    with open(args.output, "wt") as output_f, tqdm(desc="tweets", total=tweets_count) as pbar, Pool(args.cpu) as pool:
+        for i, (vector, rating) in enumerate(pool.imap_unordered(get_retweet_user, loader)):
             pbar.update()
-
-            vector, rating = get_retweet_user((tweet_id, rating))
             if vector:
                 vector = np.array(vector)
                 json_data = json.dumps([vector.tolist(), rating])
